167da16bf2221baf92de8a4efd9c538758ae44cd
Chandramani <chandramani1983@gmail.com>
2018-08-22 12:27:33 +0530

    Hello Sam

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Hellow world")
+         print("Hellow Sam")
     }
 
     override func didReceiveMemoryWarning() {
